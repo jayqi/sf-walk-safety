f65b5c8a29a9895247e1918bab4a98db76bead3e
Jay Qi <jay.y.qi@gmail.com>
2016-04-04 19:22:35 -0700

    process_data script

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -1,8 +1,16 @@
 import os
 
-path = '../rawdata/'
+def download_raw_data(filename,url):
+    path = '../rawdata/'
+    download_data(filename,url,path)
+    return 0
 
-def download_data(filename,url):
+def download_geo_data(filename,url):
+    path = '../geodata/'
+    download_data(filename,url,path)
+    return 0
+
+def download_data(filename,url,path):
     # Move old file to backup
     movecommand = r'mv '+path+filename+' '+path+filename+'.bk'
     print movecommand
@@ -18,23 +26,68 @@ def download_data(filename,url):
     print lastdownloadcommand
     print os.system(lastdownloadcommand)
 
+    return 0
+
+##########################################
+################ Raw Data ################
+##########################################
+
 def get_full_PD_data():
 
     filename = r'SFPD_Incidents_-_from_1_January_2003.csv'
     url = r"https://data.sfgov.org/api/views/tmnf-yvry/rows.csv?accessType=DOWNLOAD"
 
-    download_data(filename,url)
+    download_raw_data(filename,url)
+    return 0
 
 def get_robbery_street():
 
     filename = r'robbery-street.csv'
     url = r"https://data.sfgov.org/resource/cuks-n6tp.csv?%24select=descript%2C+dayofweek%2C+date%2C+time%2C+address%2C+x%2C+y&%24where=category+%3D+%27ROBBERY%27+AND+descript+like+%27%25ON+THE+STREET%25%27&%24limit=100000"
 
-    download_data(filename,url)
+    download_raw_data(filename,url)
+    return 0
 
 def get_theft_street():
 
     filename = r'theft-street.csv'
     url = r"https://data.sfgov.org/resource/cuks-n6tp.csv?%24select=descript%2C+dayofweek%2C+date%2C+time%2C+address%2C+x%2C+y&%24where=category+%3D+%27LARCENY/THEFT%27+AND+%28descript+like+%27%25FROM+PERSON%25%27+OR+descript+like+%27%25PICKPOCKET%25%27+OR+descript+like+%27%25PURSESNATCH%25%27+OR+descript+%3D+%27PETTY+THEFT+OF+PROPERTY%27%29&%24limit=100000"
 
-    download_data(filename,url)
+    download_raw_data(filename,url)
+    return 0
+
+##########################################
+################ Geo Data ################
+##########################################
+
+def get_neighborhoods():
+
+    filename = r'neighborhoods.geojson'
+    url = r"https://data.sfgov.org/api/geospatial/p5b7-5n3h?method=export&format=GeoJSON"
+
+    download_geo_data(filename,url)
+    return 0
+
+def get_census_tracts():
+
+    filename = r'census-tracts.geojson'
+    url = r"https://data.sfgov.org/api/geospatial/bwbp-wk3r?method=export&format=GeoJSON"
+
+    download_geo_data(filename,url)
+    return 0
+
+def get_police_districts():
+
+    filename = r'police-districts.geojson'
+    url = r"https://data.sfgov.org/api/geospatial/wkhw-cjsf?method=export&format=GeoJSON"
+
+    download_geo_data(filename,url)
+    return 0
+
+def get_historical_police_districts():
+
+    filename = r'hist-police-districts.geojson'
+    url = r"https://data.sfgov.org/api/geospatial/embj-38bg?method=export&format=GeoJSON"
+
+    download_geo_data(filename,url)
+    return 0
---
+++ scripts/process_data.py
@@ -0,0 +1,154 @@
+import pandas as pd
+import datetime as dt
+import json
+from shapely.geometry import shape, Point
+from collections import OrderedDict
+
+# Global counter for running a job
+__counter__ = 0
+
+###########################################
+############## Open Geo Data ##############
+###########################################
+
+def open_geo_data(filename):
+    path = r"../geodata/"
+
+    with open(path+filename) as f:
+        return json.load(f)
+
+def open_neighborhood_data():
+    geojson = open_geo_data('neighborhoods.geojson')
+
+    neighborhoods = {}
+    for feature in geojson['features']:
+        neighborhoods[feature['properties']['nhood']] = shape(feature['geometry'])
+
+    return neighborhoods
+
+def open_census_tract_data():
+    geojson = open_geo_data('census-tracts.geojson')
+
+    census_tracts = {}
+    for feature in geojson['features']:
+        census_tracts[feature['properties']['tractce10']] = shape(feature['geometry'])
+
+    return census_tracts
+
+def open_police_district_data():
+    geojson = open_geo_data('police-districts.geojson')
+
+    police_districts = {}
+    for feature in geojson['features']:
+        police_districts[feature['properties']['district']] = shape(feature['geometry'])
+
+    return police_districts
+
+def open_hist_police_district_data():
+    geojson = open_geo_data('hist-police-districts.geojson')
+
+    hist_police_districts = {}
+    for feature in geojson['features']:
+        hist_police_districts[feature['properties']['district']] = shape(feature['geometry'])
+
+    return hist_police_districts
+
+
+
+###########################################
+############## Open Raw Data ##############
+###########################################
+
+def open_raw_robbery_street():
+    path = r"../rawdata/"
+    filename = r"robbery-street.csv"
+
+    conv = {
+        'date' : lambda x: dt.datetime.strptime(x,"%Y-%m-%dT%H:%M:%S.%f").date(),
+        'time' : lambda x: dt.datetime.strptime(x,"%H:%M").time()
+    }
+
+    df = pd.read_csv(path+filename,converters = conv)
+    return df
+
+def open_raw_theft_street():
+    path = r"../rawdata/"
+    filename = r"theft-street.csv"
+
+    conv = {
+        'date' : lambda x: dt.datetime.strptime(x,"%Y-%m-%dT%H:%M:%S.%f").date(),
+        'time' : lambda x: dt.datetime.strptime(x,"%H:%M").time()
+    }
+
+    df = pd.read_csv(path+filename,converters = conv)
+    return df
+
+
+##########################################
+############## Process Data ##############
+##########################################
+
+def xy_to_point(row):
+    return Point(row['x'],row['y'])
+
+def identify_regions(point):
+
+    # Increment counter and print
+    global __counter__
+    __counter__ += 1
+    print __counter__
+
+    d_get = OrderedDict([
+        ('nhood' , open_neighborhood_data()),
+        ('tractce10' , open_census_tract_data()),
+        ('police_district' , open_police_district_data()),
+        ('hist_police_district' , open_hist_police_district_data())
+        ])
+
+    d_id = OrderedDict()
+
+
+    # Iterate through all region types
+    for region_type, region_dict in d_get.iteritems():
+
+        print "...identifying " + region_type + "..."
+
+        found = False
+
+        # Iterate through each region polygon
+        for region_id, polygon in region_dict.iteritems():
+
+            # If found containing polygon
+            if polygon.contains(point):
+                found = True
+                print "...found!"
+                d_id[region_type] = region_id
+
+        # If not found, put None for that region type
+        if not found:
+            d_id[region_type] = None
+
+
+
+    return pd.Series([value for key, value in d_id.iteritems()],
+                     index=[key for key,value in d_id.iteritems()])
+
+
+def add_regions(df):
+    return df.join(df.apply(xy_to_point,axis=1).apply(identify_regions))
+
+
+
+def process_robbery_street():
+    global __counter__
+    __counter__ = 0
+    path = r'../data/robbery-street.p'
+    add_regions(open_raw_robbery_street()).to_pickle(path)
+    return 0
+
+def process_theft_street():
+    global __counter__
+    __counter__ = 0
+    path = r'../data/theft-street.p'
+    add_regions(open_raw_robbery_street()).to_pickle(path)
+    return 0
