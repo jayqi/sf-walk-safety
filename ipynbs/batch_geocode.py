49eabaceb3f733ed0a289762fc4c34f160f1f581
Jay Qi <jay.y.qi@gmail.com>
2016-04-20 20:27:18 -0700

    Traffic geocoding

---
+++ ipynbs/batch_geocode.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import time
+from geopy.geocoders import GoogleV3
+import pickle
+
+def process_batch(filename):
+    batch = pickle.load( open( filename+".p", "rb" ) )
+    g = GoogleV3()
+    out = []
+    for item in batch.itertuples():
+        address = item[1]+' at '+item[2]+', San Francisco, CA'
+        location = g.geocode(address)
+        out.append((item[0],item[1],item[2],location.latitude, location.longitude))
+        time.sleep(1)
+
+    pickle.dump( batch, open( filename+"-out.p", "wb" ) )
