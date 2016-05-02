20d573c5fdbb7c107ef9882795bfcc27045c93d7
Jay Qi <jay.y.qi@gmail.com>
2016-05-02 14:30:31 -0700

    Used Frozen-Flask to build frozen static site

--- flask/run.py
+++ flask/run.py
@@ -1,6 +1,12 @@
 # Runs web app
 from application import app
+from flask_frozen import Freezer
+import sys
 
 if __name__ == '__main__':
-    port = 8080
-    app.run('0.0.0.0', port=port)
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+         freezer = Freezer(app)
+         freezer.freeze()
+    else:
+        port = 8080
+        app.run('0.0.0.0', port=port)
--- static/robbery-street-proto/blank-script.js
+++
@@ -1,5 +0,0 @@
-<!-- Leaflet Map -->
-
-L.mapbox.accessToken = 'pk.eyJ1IjoianlxaSIsImEiOiJjaW01ZG11MjcwMWl0dGttM2R2c2JvbThiIn0.eE6Xt0dMIl7Y2tOT8e6dyQ';
-var map = L.mapbox.map('map', 'mapbox.streets')
-.setView([37.7833, -122.4167], 13);
