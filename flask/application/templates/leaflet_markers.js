34624f1c7dbe2cc3f68d9472250ff413ca2c9051
Jay Qi <jay.y.qi@gmail.com>
2016-05-02 13:53:45 -0700

    Flask: completed basic Traffic stuff

--- flask/application/templates/leaflet_markers.js
+++ flask/application/templates/leaflet_markers.js
@@ -8,15 +8,38 @@ var map = L.mapbox.map('map', 'mapbox.streets', {
 
 map.scrollWheelZoom.disable();
 
+function build_popup(feature) {
+    var popuptext = 'Date: ' + feature.properties.date + '<br />' +
+    'Time: ' + feature.properties.time + '<br />' +
+    'Day of Week: ' + feature.properties.dayofweek + '<br />'
+    {% if ('/thefts/' in request.path) or ('/robberies/' in request.path) %}
+    +
+    'Location: ' + feature.properties.address + '<br />' +
+    'Description: ' + feature.properties.descript
+    {% elif '/trafficcollisions/' in request.path %}
+    +
+    'Location: ' + feature.properties.location + '<br />' +
+    '<br />' +
+    'Primary Collision Factor: ' + feature.properties.PRIMARY_COLLISION_FACTOR + '<br />' +
+    'Weather: ' + feature.properties.WEATHER + '<br />' +
+    'Road surface: ' + feature.properties.ROAD_SURFACE + '<br />' +
+    'Road condition: ' + feature.properties.ROAD_COND + '<br />' +
+    'Lighting: ' + feature.properties.LIGHTING + '<br />' +
+    ((feature.properties.HIT_AND_RUN != 'No') ? feature.properties.HIT_AND_RUN + '<br />': '') +
+    ((feature.properties.ALCOHOL_INVOLVED) ? 'Alcohol Involved' + '<br />': '') +
+    '<br />' +
+    'Pedestrian action: ' + feature.properties.PED_ACTION +
+    ((feature.properties.COUNT_PED_INJURED > 0) ? '<br /> Pedestrians injured: ' + feature.properties.COUNT_PED_INJURED : '') +
+    ((feature.properties.COUNT_PED_KILLED > 0) ? '<br /> Pedestrians injured: ' + feature.properties.COUNT_PED_KILLED : '') +
+    ''
+    {% endif %}
+    ;
+    return popuptext;
+}
+
 var geoJsonLayer = L.geoJson(data, {
    onEachFeature: function(feature, layer) {
-      layer.bindPopup(
-          'Date: ' + feature.properties.date + '<br />' +
-          'Time: ' + feature.properties.time + '<br />' +
-          'Day of Week: ' + feature.properties.dayofweek + '<br />' +
-          'Location: ' + feature.properties.address + '<br />' +
-          'Description: ' + feature.properties.descript);
-   }
+      layer.bindPopup(build_popup(feature));}
 });
 
 var markers_cluster = new L.MarkerClusterGroup({
